496252373da9de77a6083735574b7bbb327a6fe8
JamiMKorhonen <jami.m.korhonen@gmail.com>
2025-09-11 15:42:19 +0300

    Initial commit

---
+++ Tasks/Week1/Cheat Sheet2.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Here we are")
+
+Sentence = "Finding substring"
+print(Sentence)
+print(Sentence[0])
+print(Sentence[-2])
+print(Sentence[2:9])
+print(Sentence[:9])
+print(Sentence[::3])
+print(Sentence[-9:-2])
+print(Sentence[::-1])
+print(Sentence[:-1])
+
