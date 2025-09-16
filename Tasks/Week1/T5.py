2ccbf2951ce93171ae09d25cbf3b62669fb6f92c
JamiMKorhonen <jami.m.korhonen@gmail.com>
2025-09-16 05:17:27 +0300

    Added Python files of Tasks 5 & 6

---
+++ Tasks/Week1/T5.py
@@ -0,0 +1,12 @@
+print("Calculate the area of a wall.")
+
+Feed = input("Enter the width in meters: ")
+Width = int(Feed)
+
+Feed = input("Enter the height in meters: ")
+Height = int(Feed)
+
+Area = Width * Height
+
+print("Width is", Width, "m and height is", Height, "m.")
+print("The wall will be", Area, "square meters.")
