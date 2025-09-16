42f843b0ecb5f7fcd39acc8f98fb2208dd684978
JamiMKorhonen <jami.m.korhonen@gmail.com>
2025-09-16 04:22:17 +0300

    Rename tasks

---
+++ Tasks/Week1/A1_T1.py
@@ -0,0 +1,5 @@
+# Hello Python program!
+#     Write filename to be “A1_T1.py”
+#     Make sure the file extension is selected to be “.py” (Save as type: Python files)
+
+print("Hello Python program!")
