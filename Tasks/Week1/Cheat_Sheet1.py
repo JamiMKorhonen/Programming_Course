15d96341117c659171ab6a2b5e7a8c393d335d8e
JamiMKorhonen <jami.m.korhonen@gmail.com>
2025-10-01 17:46:02 +0300

    Fixed Week1 Cheat_sheet1 and added Week3 Cheat_sheet2

---
+++ Tasks/Week1/Cheat_Sheet1.py
@@ -0,0 +1,55 @@
+# #
+
+# print("Hello World!")
+# print("Here we are")
+
+
+# #
+
+# print("He said: \"Hello\", and kept walking")
+# print("He said hello \n \r \t \b and \\'kept walking")
+
+
+# # 
+
+# exampleString = "Text and numb3rs"
+# exampleInt = 12
+# exampleFloat = 12.12
+# exampleBoolean = False
+
+
+# # 
+
+# Firstname = input("First name: ")
+# Lastname = input("Last name: ")
+# print("Hello ", Firstname, Lastname)
+
+
+# #
+
+# Sentence = "Finding substring"
+# print(Sentence)
+# print(Sentence[0])
+# print(Sentence[-2])
+# print(Sentence[2:9])
+# print(Sentence[:9])
+# print(Sentence[::3])
+# print(Sentence[-9:-2])
+# print(Sentence[::-1])
+# print(Sentence[:-1])
+
+
+# #
+
+# name = "Name"
+# age = 25
+
+# print(name + " " + str(age))
+
+# #
+
+# num1 = input("Number 1: ")
+# num2 = input("Number 2: ")
+
+# num3 = int(num1) + int(num2)
+# print(num3)
---
+++ Tasks/Week3/Cheat_Sheet2.py
@@ -0,0 +1,39 @@
+# # 
+
+# print("Welcome to the themp app!")
+# Temp = int(input("What is the temperature of the CPU? "))
+# if(Temp > 80):
+#     if(Temp > 90):
+#         print("You are toast")
+#     else:
+#         print("Warning, temperature too high!")
+# elif(Temp > 60):
+#     print("Medium temperature")
+# else:
+#     print("All cool, keep on going!")
+
+
+# #
+
+# num = int(input("Insert number: "))
+# answ = num % 2
+# if answ == 0:
+#     print(f"Even number: {answ}")
+# else:
+#     print(f"Odd number, remainder when didived by 2: {answ}")
+
+
+# # 
+
+# word1 = (input("Word 1: "))
+# word2 = (input("Word 2: "))
+
+# length1 = len(word1)
+# length2 = len(word2)
+
+# if length1 == length2:
+#     print("Sama")
+# elif length1 > length2:
+#     print("Word 1 is longer")
+# elif length2 > length1:
+#     print("Word 2 is longer")
